f6181ac68139cc49a6e1d1caea0cf9491c06aac2
Bruno <bruno1_oliveira@hotmail.com>
2022-08-12 08:57:13 -0300

    atualizacao index e custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,22 @@
 // instancia jQuery e evita conflitos
-jQuery(function($){
-    
+// jQuery(function($){
+ $(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel();
+
     let titulos = $('h4') // tag
 
     let itens = $('.featured-item') //class
     
     let features = $('#featured') //id
 
+    console.log(titulos.first());
+
+    // Configuração de produtos
+
+    $('.featured-item a').addClass('btn btn-dark stretch-link');
+
+    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+
 })
 
-console.log($('h4').text());
