a7749c278631811ef83dd59da22a0d2685ca23e3
Bruno <bruno1_oliveira@hotmail.com>
2022-08-18 06:44:45 -0300

    atualizacao com criacao de modals, animaçcoes e form

--- js/custom.js
+++ js/custom.js
@@ -65,7 +65,51 @@
             console.log( $(this).find('h4').text() + ' em estoque')
         })
 
+    /* 
+    * Animações
+    */
+    const duracao = 1000 //
+
+    $('.featured-item:nth(0)')
+        .hide(duracao)
+        .show(duracao)
+        .fadeOut(duracao)
+        .fadeIn(duracao)
+        .toggle(duracao)
+        .toggle(duracao)
+
+    $('#form-submit').on('click', function(e){
+
+        e.preventDefault()
+
+        if($('#email').val() != '' ){
+            $('#email').animate({
+                opacity: "toggle",
+                top: "-50"
+            }, duracao, function(){
+                console.log($(this).val())
+            })
+
+        }
+
+    })
 
     });
 
+    /*
+    * Ouvinte de eventos .nav-modal-open
+    */
+    $('nav-modal-open').on('click', function(e){
 
+        e.preventDefault();
+
+        let elem = $(this).attr('rel')
+
+        $('.modal-body').html($('#'+elem).html())
+        $('.modal-header h5.modal-title').html($(this).text())
+
+        let myModal = new bootstrap.Modal($('#modelId'))
+
+        myModal.show()
+
+    })
